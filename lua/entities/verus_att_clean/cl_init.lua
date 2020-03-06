61d663c6e52c7ca3689a71b0158228c051a9bff9
Nicolas Wasserkampf <nicolaswasserkampf@gmail.com>
2020-03-06 20:04:58 +0100

    display name for att

--- lua/entities/verus_att_clean/cl_init.lua
+++ lua/entities/verus_att_clean/cl_init.lua
@@ -2,4 +2,12 @@ include("entities/verus_att_clean/shared.lua")
 
 function ENT:Draw()
     self:DrawModel()
+    local ply = LocalPlayer()
+    local pos = self:GetPos()
+    local eang = (pos - ply:GetPos()):Angle()
+    eang:RotateAroundAxis(eang:Up(), -90)
+    eang.r = 90
+    cam.Start3D2D(pos + Vector(0, 0, 20), eang, 0.15)
+    draw.WordBox(2, -85, 0, self.PrintName, "ChatFont", Color(0, 0, 0, 200), Color(255, 255, 255, 255))
+    cam.End3D2D()
 end
--- lua/entities/verus_barrel_supp/cl_init.lua
+++ lua/entities/verus_barrel_supp/cl_init.lua
@@ -16,4 +16,10 @@ function ENT:Draw()
     end
 
     cam.End3D2D()
+    local eang = (pos - ply:GetPos()):Angle()
+    eang:RotateAroundAxis(eang:Up(), -90)
+    eang.r = 90
+    cam.Start3D2D(pos + Vector(0, 0, 20), eang, 0.15)
+    draw.WordBox(2, -45, 0, self.PrintName, "ChatFont", Color(0, 0, 0, 200), Color(255, 255, 255, 255))
+    cam.End3D2D()
 end
